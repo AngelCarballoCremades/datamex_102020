efdbf02a04131d7db7d89ead3974d1b372f75973
Angel Carballo Cremades <a.carcre@gmail.com>
2020-10-21 17:43:25 -0500

    Añadiendo lab_numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,69 +1,74 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+# print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random(size=[2,3,5])
 
+# a = np.random.rand(2,3,5)
+# a = np.random.randint(0,10000,size = (2,3,5)) #random integers
+# a = np.random.uniform(0,1,size = (2,3,5))
 
 #4. Print a.
 
+# print(a)
 
-
-#5. Create a 5x2x3 3-dimensional array with all values equaling 1.
+#5. Create a 5x3x2 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,3,2))
 
 #6. Print b.
 
-
+# print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+# print(a.shape == b.shape)
+# print(f'a shape = {a.shape}\nb shape = {b.shape}')
 
 #8. Are you able to add a and b? Why or why not?
 
-
+# I'm not able because a and b have different shape
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = b.T
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
-
+# print(a)
+# print(d)
+# d values are a's values plus 1
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
 
 #13. Does e equal to a? Why or why not?
 
-
-
+# Yes, they are the same, because a*c just multiplied every value of a times 1, so e is essentially a
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-
+f = np.empty((2,3,5))
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -75,7 +80,19 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+for matrix in range(d.shape[0]):
+    for row in range(d.shape[1]):
+        for value in range(d.shape[2]):
+            if d_min < d[matrix,row,value] < d_mean:
+                f[matrix,row,value] = 25
+            elif d_mean < d[matrix,row,value] < d_max:
+                f[matrix,row,value] = 75
+            elif d[matrix,row,value] == d_mean:
+                f[matrix,row,value] = 50
+            elif d[matrix,row,value] == d_min:
+                f[matrix,row,value] = 0
+            elif d[matrix,row,value] == d_max:
+                f[matrix,row,value] = 100
 
 
 """
@@ -98,10 +115,12 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+# print(d)
+# print('')
+# print(f)
 
 """
-#18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
+#18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values
 ("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
 array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'B',  'B',  'B'],
@@ -112,3 +131,24 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+# In order to have strings in an array the type of the array must be defined when creating it
+g = np.empty((2,3,5),dtype=str)
+
+
+for matrix in range(d.shape[0]):
+    for row in range(d.shape[1]):
+        for value in range(d.shape[2]):
+            if d_min < d[matrix, row, value] < d_mean:
+                g[matrix, row, value] = 'B'
+            elif d_mean < d[matrix, row, value] < d_max:
+                g[matrix, row, value] = 'D'
+            elif d[matrix, row, value] == d_mean:
+                g[matrix, row, value] = 'C'
+            elif d[matrix, row, value] == d_min:
+                g[matrix, row, value] = 'A'
+            elif d[matrix, row, value] == d_max:
+                g[matrix, row, value] = 'E'
+
+print(d)
+print('')
+print(g)
