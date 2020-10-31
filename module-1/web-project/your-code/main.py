811ac51c03d46aeb79e147be8884c50c13e91ffc
Angel Carballo Cremades <a.carcre@gmail.com>
2020-10-31 12:12:27 -0600

    Modificando main.py, sin funcionar aún

--- module-1/web-project/your-code/main.py
+++ module-1/web-project/your-code/main.py
@@ -8,73 +8,68 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # print([os.getcwd()])
-
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.dir", 'C:\\Users\\Angel\\Desktop')
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
-
-
-driver = webdriver.Firefox(firefox_profile=profile)
-
-driver.get('https://www.cenace.gob.mx/Paginas/SIM/Reportes/H_PreciosEnergiaSisMEM.aspx?N=5&opc=divCssPreEnergia&site=Precios%20de%20la%20energ%C3%ADa/Precios%20Marginales%20Locales/MDA/Diarios&tipoArch=C&tipoUni=SIN&tipo=Diarios&nombrenodop=Precios%20Marginales%20Locales')
-
-driver.find_element_by_xpath('/html/body/form/div[4]/div[1]/div/div[1]/div[2]/div/table/tbody/tr/td[1]/div/ul/li[1]/div/span[3]').click()
-
-xpaths = ['/html/body/form/div[4]/div[1]/div/div[1]/div[2]/div/table/tbody/tr/td[3]/div[1]/div/table/tbody/tr[2]/td[2]/table/tbody/tr[1]/td[2]/a[1]/img',
-'/html/body/form/div[4]/div[1]/div/div[1]/div[2]/div/table/tbody/tr/td[3]/div[1]/div/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td[2]/a[1]/img',
-'/html/body/form/div[4]/div[1]/div/div[1]/div[2]/div/table/tbody/tr/td[3]/div[1]/div/table/tbody/tr[4]/td[2]/table/tbody/tr[11]/td[2]/a[1]/img']
-
-main_handle = driver.current_window_handle
-print(main_handle)
-
-file_selector = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, xpaths[0])))
-
-file_selector.click()
+download_folder = 'C:\\Users\\Angel\\Desktop\\files'
+main_url = 'https://www.cenace.gob.mx/Paginas/SIM/Reportes/H_PreciosEnergiaSisMEM.aspx?N=5&opc=divCssPreEnergia&site=Precios%20de%20la%20energ%C3%ADa/Precios%20Marginales%20Locales/MDA/Diarios&tipoArch=C&tipoUni=SIN&tipo=Mensuales&nombrenodop=Precios%20Marginales%20Locales'
+xpath_timelapse = '/html/body/form/div[4]/div[1]/div/div[1]/div[2]/div/table/tbody/tr/td[1]/div/ul/li[2]/div/span[3]'
+element_dict = {'file':'/td[2]/a[1]/img', 'month':'/td[1]','date':'/td[3]'}
+xpath = '/html/body/form/div[4]/div[1]/div/div[1]/div[2]/div/table/tbody/tr/td[3]/div[1]/div/table/tbody/tr[{table_2en2}]/td[2]/table/tbody/tr[{month_period_12}]{element_dict[]}'
 
 
-WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
+def open_driver():
 
-time.sleep(5)
-wait = True
-while wait:
-    wait = False
-    for file in os.listdir('C:\\Users\\Angel\\Desktop'):
-        if ".part" in file:
-            time.sleep(0.5)
-            wait = True
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.dir", download_folder)
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+    print('Opening Browser.')
+    driver = webdriver.Firefox(firefox_profile=profile)
 
 
+# driver.get('https://www.cenace.gob.mx/Paginas/SIM/Reportes/H_PreciosEnergiaSisMEM.aspx?N=5&opc=divCssPreEnergia&site=Precios%20de%20la%20energ%C3%ADa/Precios%20Marginales%20Locales/MDA/Diarios&tipoArch=C&tipoUni=SIN&tipo=Diarios&nombrenodop=Precios%20Marginales%20Locales')
 
-windows_after = driver.window_handles
-print(windows_after)
-new_window = [x for x in windows_after if x != main_handle][0]
-# driver.switch_to_window(new_window) <!---deprecated>
-driver.switch_to.window(new_window)
+def get_url(url):
+    driver.get(url)
 
-file_selector = find_element_by_xpath(xpaths[1])
+def find_and_click(element_xpath,*i = 0):
+    if i == 0:
+        driver.WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, element_xpath))).click()
+    else:
+        driver.WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, element_xpath.format(i[0],i[1])))).click()
 
-file_selector.click()
 
+def wait_download(directorio,i):
+
+    while directorio == os.listdir(download_folder):
+        pass
+
+    time.sleep(1)
+    print(f'Descargando {i}', end = '')
+
+    wait = True
+    while wait:
+        wait = False
+        for file in os.listdir(download_folder):
+            if ".part" in file:
+                time.sleep(0.5)
+                wait = True
+                print('.', end = '')
+    print('Listo')
 
 
 
+open_driver()
+get_url(main_url)
+find_and_click(xpath_timelapse)
 
-# for xpath in xpaths:
-#     # pdb.set_trace()
-#     file_selector = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, xpath)))
+print('Inicio')
+for table in range(2,73,2):
+    find_and_click()
+    directorio = os.listdir(download_folder)
 
-#     file_selector.click()
+    file_selector.click()
 
-#     driver.switchTo().window(main_handle)
-#     # wait = True
-#     # while wait:
-#     #     wait = False
-    #     for file in os.listdir('C:\\Users\\Angel\\Desktop'):
-    #         if ".part" in file:
-    #             time.sleep(0.5)
-    #             wait = True
+    wait_download(directorio,i)
 
+print(f'Listos {i}')
 
-
-# driver.quit()
+driver.quit()
